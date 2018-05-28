33f8a504f97971b99f734667a83ad813e9da1458
jaemin.han <jaemin.han@nyu.edu>
2018-05-27 20:25:23 -0400

    initial setup and confirming render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends React.Component {
+  render() {
+    // const people = [
+    //   { name: "Jae" },
+    //   { name: "Min" },
+    //   { name: "Han" }
+    // ]
+
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts= {[
+          { name: "Jae" },
+          { name: "Min" },
+          { name: "Han" }
+        ]}/>
+        <ContactList contacts= {[
+          { name: "One" },
+          { name: "Two" },
+          { name: "Three" }
+        ]}/>
+        <ContactList contacts= {[
+          { name: "Company" },
+          { name: "Laptop" },
+          { name: "Phone" }
+        ]}/>
       </div>
     );
   }
