c566fa7d994c7c53eea9ab45132d3f5eb414edc1
jaemin.han <jaemin.han@nyu.edu>
2018-05-28 17:17:41 -0400

    Listing contacts in ListContacts.js file and console.log

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
-class ContactList extends React.Component {
-  render() {
-    // const people = [
-    //   { name: "Jae" },
-    //   { name: "Min" },
-    //   { name: "Han" }
-    // ]
-
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
-
+]
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts= {[
-          { name: "Jae" },
-          { name: "Min" },
-          { name: "Han" }
-        ]}/>
-        <ContactList contacts= {[
-          { name: "One" },
-          { name: "Two" },
-          { name: "Three" }
-        ]}/>
-        <ContactList contacts= {[
-          { name: "Company" },
-          { name: "Laptop" },
-          { name: "Phone" }
-        ]}/>
+      <div>
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
