bf07ad153d4569fb4c7f16ea2b34c67357bfbe12
jaemin.han <jaemin.han@nyu.edu>
2018-05-28 21:21:35 -0400

    protypes and controlled components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class ListContacts extends Component {
-    render() {
-        // console.log("Props", this.props)
-        return (
-            <ol className="contact-list">
-                {this.props.contacts.map((contact) => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}
-                        ></div>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button className='contact-remove'>
+function ListContacts (props) {
+    // console.log("Props", this.props)
+    return (
+        <ol className="contact-list">
+            {props.contacts.map((contact) => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div
+                        className='contact-avatar'
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    ></div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button 
+                        onClick={() => props.onDeleteContact(contact)}
+                        className='contact-remove'>
                             Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
+}
+
+ListContacts.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
 }
 
 export default ListContacts
