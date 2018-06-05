498ec25f5ce5f1f03c63096331257dee99560e77
jaemin.han <jaemin.han@nyu.edu>
2018-06-05 17:10:32 -0400

    createcontact modal styling in progress before react scss implementation

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -16,24 +16,26 @@ class CreateContact extends Component {
     };
     render() {
         return (
-            <div>
-                <Link className="close-create-contact" to="/">
-                    Close
-                </Link>
-                {/* Whenever the input field is changed, pass the handleSubmit method into onSubmit attribute */}
-                <form onSubmit={this.handleSubmit} className="create-contact-form">
-                    <ImageInput
-                        className="create-contact-avatar-input"
-                        name="avatarURL"
-                        maxHeight={64}
-                    />
-                    <div className="create-contact-details">
-                        <input type="text" name="name" placeholder="Name"/>
-                        <input type="text" name="handle" placeholder="Handle"/>
-                        <input type="text" name="description" placeholder="Description"/>
-                        <button>Add Contact</button>
-                    </div>
-                </form>
+            <div className="create-modal_container">
+                <div className="create-modal_child">
+                    <Link className="close-create-contact" to="/">
+                        Close
+                    </Link>
+                    {/* Whenever the input field is changed, pass the handleSubmit method into onSubmit attribute */}
+                    <form onSubmit={this.handleSubmit} className="create-contact-form">
+                        <ImageInput
+                            className="create-contact-avatar-input"
+                            name="avatarURL"
+                            maxHeight={64}
+                        />
+                        <div className="create-contact-details">
+                            <input type="text" name="name" placeholder="Name"/>
+                            <input type="text" name="handle" placeholder="Handle"/>
+                            <input type="text" name="description" placeholder="Description"/>
+                            <button>Add Contact</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
