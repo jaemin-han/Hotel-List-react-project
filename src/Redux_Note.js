00ca3abec3184590a346bb9aeac8791dbcf04c25
jaemin.han <jaemin.han@nyu.edu>
2018-06-25 21:52:27 -0400

    redux practice - leveraging constants

--- src/Redux_Note.js
+++ src/Redux_Note.js
@@ -1,52 +1,7 @@
-
-
-
-// State and Action (Object/Action - change state of the store)
-// 'Type' Property
-{
-    type: 'ADD_TODO';
-    todo: {
-        id: 0;
-        name: 'Learn Redux';
-        complete: false;
-    }
-}
-
-{
-    type: 'REMOVE_TODO';
-    id: 0;
-}
-
-{
-    type: 'TOGGLE_TODO';
-    id: 0;
-}
-
-{
-    type: 'ADD_GOAL';
-    goal: {
-        id: 0;
-        name: 'Run a Marathon'
-    }
-}
-
-{
-    type: 'REMOVE_GOAL';
-    id: 0;
-}
-
 // Charateristics of a Pure Function
 // 1. They always return the same result if they same arguments are passed in.
 // 2. They depend only on the arguments passed into them.
 // 3. Never produce any side effects.
-
-function todos (state = [], action) {
-    if (action.type === 'ADD_TODO') {
-        return state.concat([action.todo])
-    }
-  
-    return state
-  }
 
 function createStore (reducer) {
     // The store should have four parts
@@ -94,6 +49,111 @@ function createStore (reducer) {
 
     }
 }
+
+// App Code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+// Reducer function
+function todos (state = [], action) {
+    switch(action.type) {
+      case ADD_TODO :
+        return state.concat([action.todo])
+      case REMOVE_TODO :
+        return state.filter((todo) => todo.id !== action.id)
+      case TOGGLE_TODO :
+        return state.map((todo) => todo.id !== action.id ? todo :
+          Object.assign({}, todo, {complete: !todo.complete})
+        )
+      default :
+        return state
+    }
+  }
+  
+  function goals (state = [], action) {
+    switch(action.type) {
+      case ADD_GOAL :
+        return state.concat([action.goal])
+      case REMOVE_GOAL :
+        return state.filter((goal) => goal.id !== action.id)
+      default :
+        return state
+    }
+  }
+  
+  function app (state = {}, action) {
+    return {
+      todos: todos(state.todos, action),
+      goals: goals(state.goals, action)
+    }
+  }
+  
+  const store = createStore(app)
+  
+  store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+  })
+  
+  store.dispatch({
+    type: ADD_TODO,
+    todo: {
+      id: 0,
+      name: 'Walk the dog',
+      complete: false,
+    }
+  })
+  
+  store.dispatch({
+    type: ADD_TODO,
+    todo: {
+      id: 1,
+      name: 'Wash the car',
+      complete: false,
+    }
+  })
+  
+  store.dispatch({
+    type: ADD_TODO,
+    todo: {
+      id: 2,
+      name: 'Go to the gym',
+      complete: true,
+    }
+  })
+  
+  store.dispatch({
+    type: REMOVE_TODO,
+    id: 1
+  })
+  
+  store.dispatch({
+    type: TOGGLE_TODO,
+    id: 0
+  })
+  
+  store.dispatch({
+    type: ADD_GOAL,
+    goal: {
+      id: 0,
+      name: 'Learn Redux'
+    }
+  })
+  
+  store.dispatch({
+    type: ADD_GOAL,
+    goal: {
+      id: 1,
+      name: 'Lose 20 pounds'
+    }
+  })
+  
+  store.dispatch({
+    type: REMOVE_GOAL,
+    id: 0
+  })
 
 const store = createStore()
 // Call store.dispatch, pass the action, dispatch function is going to be invoked
